9dc021011b81a2ddd81d0d13ef662cbe105bb3f1
JonesRandom <jones.random97@gmail.com>
2017-03-29 15:08:19 +0800

    Refactor

--- app/src/main/java/com/jonesrandom/testranslateapi/MainActivity.java
+++ app/src/main/java/com/jonesrandom/testranslateapi/MainActivity.java
@@ -3,26 +3,13 @@ package com.jonesrandom.testranslateapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import com.jonesrandom.testranslateapi.Common.SpinnerAdapter;
 import com.jonesrandom.testranslateapi.ModelTranslate.ModelLanguage;
 import com.jonesrandom.testranslateapi.MyInterfaces.ResponseListener;
 import com.jonesrandom.testranslateapi.Translate.RequestLanguageData;
 import com.jonesrandom.testranslateapi.Translate.RequestTranslateData;
 
 import java.util.List;
-
-import butterknife.BindString;
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
